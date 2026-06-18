78ca4f043685c0ba6d273d84e9c87cf26e9ae3bc
Bhavesh Jat <bhveshjat2006@gmail.com>
2026-06-18 15:45:00 +0530

    basic setup

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {Server} from "socket.io";
+import {createServer} from "http";
+
+const port=3000;
+const app= express();
+
+const server= createServer(app);
+const io= new Server(server);
+
+app.get("/", (req,res)=>{
+    res.send("Hello World!");
+});
+
+io.on("connection", (socket)=>{
+    console.log("socket id is: ", socket.id);
+});
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+});
+
+
+
+
