dc1e2098de264a8874456a8a3b8b77f4450ec775
Bhavesh Jat <bhveshjat2006@gmail.com>
2026-06-18 19:20:53 +0530

    connection built

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import {Server} from "socket.io";
 import {createServer} from "http";
+import cors from "cors";
 
 const port=3000;
 const app= express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
 const server= createServer(app);
 const io= new Server(server, ({
     cors:{
-        origin:"http://localhost:5173/",
+        origin:"http://localhost:5173",
         methods:["GET", "POST"],
         credentials:true
     }
@@ -18,8 +24,10 @@ app.get("/", (req,res)=>{
     res.send("Hello World!");
 });
 
-io.on("connection", (socket)=>{
+io.on("connect", (socket)=>{
+    console.log("client connected!");
     console.log("socket id is: ", socket.id);
+    socket.broadcast.emit("welcome", `welcome to the server, ${socket.id}`);
 });
 
 server.listen(port, ()=>{
